55cf9256661c9348739d53df4de9e17d27b5c195
cScarlson <otocarlson@gmail.com>
2021-12-01 21:06:38 -0700

    cleanup templaters#templateRepeat

--- src/templaters.ts
+++ src/templaters.ts
@@ -4,29 +4,30 @@ const templateRepeat = (function getTemplateRepeat() {
     
     return function templateRepeat(template: string, data: any): string {
         if ( !/\*for/.test(template) ) return template;
-        var container = prepare(template);
-        var repeater = container.querySelector('[_for]') || document.createElement('div');
-        var repeaterHTML = repeater.outerHTML;
-        var [ property, value, vars, indexName, datumName, list ] = /_for="(let\s(\[(\w+),\s*(\w+)\])\sof\s([^"]+))"/.exec(repeaterHTML) || [];
+        var fragment = prepare(template);
+        var target = fragment.querySelector('[_for]');
+        var html = target.outerHTML;
+        var [ property, value, vars, indexName, datumName, list ] = /_for="(let\s(\[(\w+),\s*(\w+)\])\sof\s([^"]+))"/.exec(html) || [];
         var items = (new Function(`with (this) return ${list}`)).call(data);
         var elements = items.reduce(reduce, '');
         
         function prepare(template: string): HTMLDivElement {
-            parser.innerHTML = template.replace('*for', '_for');  // legalize name for selection. only replace first instance of "*for".
+            var selectable = template.replace('*for', '_for');  // legalize name for selection. only replace first instance of "*for".
+            parser.innerHTML = selectable;
             return parser;
         }
         
         function reduce(copies: string, subject: string, i: number): string {
-            var groomed = repeaterHTML
-            , groomed = groomed.replace(property, `[${datumName}]="${list}[${i}]" [${indexName}]="${i}"`)
-            , groomed = groomed.replaceAll(`{${datumName}}`, `${list}[${i}]`)
-            , groomed = groomed.replaceAll(`{${indexName}}`, `${i}`)
-            ;
+            var groomed = html
+              , groomed = groomed.replace(property, `[${datumName}]="${list}[${i}]" [${indexName}]="${i}"`)
+              , groomed = groomed.replaceAll(`{${datumName}}`, `${list}[${i}]`)
+              , groomed = groomed.replaceAll(`{${indexName}}`, `${i}`)
+              ;
             return `${copies}\n${groomed}`;
         }
         
-        repeater.outerHTML = elements;  // replace single element with multiple.
-        template = container.outerHTML;
+        target.outerHTML = elements;  // replace single element with multiple.
+        template = fragment.outerHTML;
         
         return templateRepeat(template, data);
     };
